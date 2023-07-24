6f2551b6fd2de4e59a7017c75fdbf577df60f547
Dow Drake <dowdrake@gmail.com>
2023-07-23 18:03:55 -0700

    Update for Python3 and PEP 8 standard.

--- demos/odyssey/fodyssey.py
+++ demos/odyssey/fodyssey.py
@@ -1,11 +1,14 @@
-#Copyright ReportLab Europe Ltd. 2000-2008
-#see license.txt for license details
-__version__=''' $Id: fodyssey.py 3660 2010-02-08 18:17:33Z damian $ '''
-__doc__=''
+# Copyright ReportLab Europe Ltd. 2000-2008
+# see license.txt for license details
+__version__ = ''' $Id: fodyssey.py 3660 2010-02-08 18:17:33Z damian $ '''
+__doc__ = ''
 
-#REPORTLAB_TEST_SCRIPT
-import sys, copy, os
-from reportlab.platypus import *
+# REPORTLAB_TEST_SCRIPT
+import sys
+import copy
+import os
+from reportlab.platypus import (SimpleDocTemplate, PageBreak, Paragraph, Spacer,
+                                Preformatted)
 from reportlab.lib.units import inch
 from reportlab.lib.styles import getSampleStyleSheet
 from reportlab.lib.enums import TA_LEFT, TA_RIGHT, TA_CENTER, TA_JUSTIFY
@@ -18,20 +21,24 @@ styles = getSampleStyleSheet()
 Title = "The Odyssey"
 Author = "Homer"
 
+
 def myFirstPage(canvas, doc):
     canvas.saveState()
     canvas.restoreState()
 
+
 def myLaterPages(canvas, doc):
     canvas.saveState()
-    canvas.setFont('Times-Roman',9)
+    canvas.setFont('Times-Roman', 9)
     canvas.drawString(inch, 0.75 * inch, "Page %d" % doc.page)
     canvas.restoreState()
 
+
 def go():
-    doc = SimpleDocTemplate('fodyssey.pdf',showBoundary='showboundary' in sys.argv)
-    doc.allowSplitting = not 'nosplitting' in sys.argv
-    doc.build(Elements,myFirstPage,myLaterPages)
+    doc = SimpleDocTemplate('fodyssey.pdf', showBoundary='showboundary' in sys.argv)
+    doc.allowSplitting = 'nosplitting' not in sys.argv
+    doc.build(Elements, myFirstPage, myLaterPages)
+
 
 Elements = []
 
@@ -43,16 +50,20 @@ InitialStyle.fontsize = 16
 InitialStyle.leading = 20
 PreStyle = styles["Code"]
 
+
 def newPage():
     Elements.append(PageBreak())
+
 
 def chapter(txt, style=ChapterStyle):
     newPage()
     Elements.append(Paragraph(txt, style))
     Elements.append(Spacer(0.2*inch, 0.3*inch))
 
-def fTitle(txt,style=InitialStyle):
+
+def fTitle(txt, style=InitialStyle):
     Elements.append(Paragraph(txt, style))
+
 
 ParaStyle = copy.deepcopy(styles["Normal"])
 ParaStyle.spaceBefore = 0.1*inch
@@ -67,94 +78,108 @@ elif 'center' in sys.argv or 'centre' in sys.argv:
 else:
     ParaStyle.alignment = TA_JUSTIFY
 
+
 def spacer(inches):
     Elements.append(Spacer(0.1*inch, inches*inch))
 
+
 def p(txt, style=ParaStyle):
     Elements.append(Paragraph(txt, style))
+
 
 def pre(txt, style=PreStyle):
     spacer(0.1)
     p = Preformatted(txt, style)
     Elements.append(p)
 
+
 def parseOdyssey(fn):
     from time import time
     E = []
-    t0=time()
-    text = open(fn,'r').read()
+    t0 = time()
+    text = open(fn, 'r').read()
     i0 = text.index('Book I')
     endMarker = 'covenant of peace between the two contending parties.'
     i1 = text.index(endMarker)+len(endMarker)
-    PREAMBLE=map(str.strip,text[0:i0].split('\n'))
-    L=map(str.strip,text[i0:i1].split('\n'))
-    POSTAMBLE=map(str.strip,text[i1:].split('\n'))
+    # map object doesn't support indexing, replace with list comprehensions
+    PREAMBLE = [s.strip() for s in text[0:i0].split('\n')]
+    L = [s.strip() for s in text[i0:i1].split('\n')]
+    POSTAMBLE = [s.strip() for s in text[i1:].split('\n')]
 
     def ambleText(L):
-        while L and not L[0]: L.pop(0)
+        while L and not L[0]:
+            L.pop(0)
         while L:
-            T=[]
+            T = []
             while L and L[0]:
                 T.append(L.pop(0))
             yield T
-            while L and not L[0]: L.pop(0)
+            while L and not L[0]:
+                L.pop(0)
 
     def mainText(L):
         while L:
             B = L.pop(0)
-            while not L[0]: L.pop(0)
-            T=[]
+            while not L[0]:
+                L.pop(0)
+            T = []
             while L and L[0]:
                 T.append(L.pop(0))
-            while not L[0]: L.pop(0)
+            while not L[0]:
+                L.pop(0)
             P = []
-            while L and not (L[0].startswith('Book ') and len(L[0].split())==2):
-                E=[]
+            while L and not (L[0].startswith('Book ') and len(L[0].split()) == 2):
+                E = []
                 while L and L[0]:
                     E.append(L.pop(0))
                 P.append(E)
                 if L:
-                    while not L[0]: L.pop(0)
-            yield B,T,P
+                    while not L[0]:
+                        L.pop(0)
+            yield B, T, P
 
     t1 = time()
-    print "open(%s,'r').read() took %.4f seconds" %(fn,t1-t0)
+    print("open(%s, 'r').read() took %.4f seconds" % (fn, t1 - t0))
 
-    E.append([spacer,2])
-    E.append([fTitle,'<font color=red>%s</font>' % Title, InitialStyle])
-    E.append([fTitle,'<font size=-4>by</font> <font color=green>%s</font>' % Author, InitialStyle])
+    E.append([spacer, 2])
+    E.append([fTitle, '<font color=red>%s</font>' % Title, InitialStyle])
+    E.append([fTitle, '<font size=-4>by</font> <font color=green>%s</font>' % Author,
+              InitialStyle])
 
     for T in ambleText(PREAMBLE):
-        E.append([p,'\n'.join(T)])
+        E.append([p, '\n'.join(T)])
 
-    for (B,T,P) in mainText(L):
-        E.append([chapter,B])
-        E.append([p,'<font size="+1" color="Blue"><b>%s</b></font>' % '\n'.join(T),ParaStyle])
+    for (B, T, P) in mainText(L):
+        E.append([chapter, B])
+        E.append([p, '<font size="+1" color="Blue"><b>%s</b></font>' % '\n'.join(T),
+                  ParaStyle])
         for x in P:
-            E.append([p,' '.join(x)])
-    firstPre = 1
+            E.append([p, ' '.join(x)])
+    # firstPre = 1 (assigned but not used)
     for T in ambleText(POSTAMBLE):
-        E.append([p,'\n'.join(T)])
+        E.append([p, '\n'.join(T)])
 
     t3 = time()
-    print "Parsing into memory took %.4f seconds" %(t3-t1)
+    print("Parsing into memory took %.4f seconds" % (t3-t1))
     del L
     t4 = time()
-    print "Deleting list of lines took %.4f seconds" %(t4-t3)
-    for i in xrange(len(E)):
+    print("Deleting list of lines took %.4f seconds" % (t4-t3))
+    for i in range(len(E)):  # no xrange in Python 3, but range gives desired behavior
         E[i][0](*E[i][1:])
     t5 = time()
-    print "Moving into platypus took %.4f seconds" %(t5-t4)
+    print("Moving into platypus took %.4f seconds" % (t5-t4))
     del E
     t6 = time()
-    print "Deleting list of actions took %.4f seconds" %(t6-t5)
+    print("Deleting list of actions took %.4f seconds" % (t6-t5))
     go()
     t7 = time()
-    print "saving to PDF took %.4f seconds" %(t7-t6)
-    print "Total run took %.4f seconds"%(t7-t0)
+    print("saving to PDF took %.4f seconds" % (t7-t6))
+    print("Total run took %.4f seconds" % (t7-t0))
 
-for fn in ('odyssey.full.txt','odyssey.txt'):
+
+for fn in ('odyssey.full.txt', 'odyssey.txt'):
     if os.path.isfile(fn):
         break
-if __name__=='__main__':
+
+if __name__ == '__main__':
     parseOdyssey(fn)
